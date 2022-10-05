376e603dec6e1b58702ce841fe45a87b4f1af17c
KathykkE <63220354+KathykkE@users.noreply.github.com>
2022-10-05 15:12:30 +0800

    Add files via upload

---
+++ homepage/lib/main.dart
@@ -0,0 +1,608 @@
+import 'package:flutter/material.dart';
+
+
+class 7homepageWidget extends StatelessWidget {
+@override
+Widget build(BuildContext context) {
+// Figma Flutter Generator 7homepageWidget - FRAME
+return Container(
+width: 428,
+height: 926,
+decoration: BoxDecoration(
+color : Color.fromRGBO(240, 238, 238, 0.10000000149011612),
+),
+child: Stack(
+children: <Widget>[
+Positioned(
+top: 0,
+left: 0,
+child: Container(
+width: 428,
+height: 926,
+decoration: BoxDecoration(
+color : Color.fromRGBO(240, 238, 238, 1),
+)
+)
+),Positioned(
+top: 814,
+left: 26,
+child: Container(
+width: 78,
+height: 73,
+decoration: BoxDecoration(
+color : Color.fromRGBO(246, 118, 25, 0.30000001192092896),
+borderRadius : BorderRadius.all(Radius.elliptical(78, 73)),
+)
+)
+),Positioned(
+top: 156,
+left: 38,
+child: Container(
+width: 167.5800018310547,
+height: 196,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Paul_heim_cordura_business_bag_1662386088_01a57a95_progressive_thumbnail1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 156,
+left: 228,
+child: Container(
+width: 167,
+height: 197.4529266357422,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Chanel_1662390860_2b62f383_progressive_thumbnail1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 374,
+left: 38,
+child: Container(
+width: 167,
+height: 195.32164001464844,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Bicycle_helmet_1659765144_8abfd9a1_progressive_thumbnail1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 374,
+left: 228,
+child: Container(
+width: 167,
+height: 200,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Auth_brand_new_cond_louis_vuit_1661618139_b522d5b8_progressive_thumbnail1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 596,
+left: 40,
+child: Container(
+width: 169,
+height: 200,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/121040_234706641_thumbnailn_progressive_thumbnail1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 596,
+left: 228,
+child: Container(
+width: 162,
+height: 200,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/_authentic_lv_key_pouch_1661589301_33f03897_progressive_thumbnail1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 828,
+left: 43,
+child: Container(
+width: 45,
+height: 43,
+decoration: BoxDecoration(
+boxShadow : [BoxShadow(
+color: Color.fromRGBO(0, 0, 0, 0.25),
+offset: Offset(0,4),
+blurRadius: 4
+)],
+image : DecorationImage(
+image: AssetImage('assets/images/950201.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 824,
+left: 341,
+child: Container(
+width: 51,
+height: 51,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/26333201.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 828,
+left: 242,
+child: Container(
+width: 44,
+height: 43,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Plussignicon1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 165,
+left: 172,
+child: Container(
+width: 30,
+height: 30,
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(100),
+topRight: Radius.circular(100),
+bottomLeft: Radius.circular(100),
+bottomRight: Radius.circular(100),
+),
+image : DecorationImage(
+image: AssetImage('assets/images/9020271.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 381,
+left: 172,
+child: Container(
+width: 30,
+height: 30,
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(100),
+topRight: Radius.circular(100),
+bottomLeft: Radius.circular(100),
+bottomRight: Radius.circular(100),
+),
+image : DecorationImage(
+image: AssetImage('assets/images/9020273.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 607,
+left: 172,
+child: Container(
+width: 30,
+height: 30,
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(100),
+topRight: Radius.circular(100),
+bottomLeft: Radius.circular(100),
+bottomRight: Radius.circular(100),
+),
+image : DecorationImage(
+image: AssetImage('assets/images/9020275.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 165,
+left: 352,
+child: Container(
+width: 30,
+height: 30,
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(100),
+topRight: Radius.circular(100),
+bottomLeft: Radius.circular(100),
+bottomRight: Radius.circular(100),
+),
+image : DecorationImage(
+image: AssetImage('assets/images/9020272.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 381,
+left: 352,
+child: Container(
+width: 30,
+height: 30,
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(100),
+topRight: Radius.circular(100),
+bottomLeft: Radius.circular(100),
+bottomRight: Radius.circular(100),
+),
+image : DecorationImage(
+image: AssetImage('assets/images/9020274.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 607,
+left: 354,
+child: Container(
+width: 30,
+height: 30,
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(100),
+topRight: Radius.circular(100),
+bottomLeft: Radius.circular(100),
+bottomRight: Radius.circular(100),
+),
+image : DecorationImage(
+image: AssetImage('assets/images/9020276.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 828,
+left: 140,
+child: Container(
+width: 47,
+height: 43,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Pngkey1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 327,
+left: 38,
+child: Container(
+width: 168,
+height: 29,
+decoration: BoxDecoration(
+boxShadow : [BoxShadow(
+color: Color.fromRGBO(0, 0, 0, 0.25),
+offset: Offset(0,4),
+blurRadius: 4
+)],
+color : Color.fromRGBO(255, 255, 255, 1),
+)
+)
+),Positioned(
+top: 333,
+left: 101,
+child: Text('Canvas Bag', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 333,
+left: 45,
+child: Text('\$23  ', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 327,
+left: 228,
+child: Container(
+width: 167,
+height: 29,
+decoration: BoxDecoration(
+boxShadow : [BoxShadow(
+color: Color.fromRGBO(0, 0, 0, 0.25),
+offset: Offset(0,4),
+blurRadius: 4
+)],
+color : Color.fromRGBO(255, 255, 255, 1),
+)
+)
+),Positioned(
+top: 333,
+left: 275,
+child: Text('Preloved Chanel ', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 333,
+left: 230,
+child: Text('\$500 ', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 545,
+left: 228,
+child: Container(
+width: 167,
+height: 29,
+decoration: BoxDecoration(
+boxShadow : [BoxShadow(
+color: Color.fromRGBO(0, 0, 0, 0.25),
+offset: Offset(0,4),
+blurRadius: 4
+)],
+color : Color.fromRGBO(255, 255, 255, 1),
+)
+)
+),Positioned(
+top: 550,
+left: 238,
+child: Text('\$300 ', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 767,
+left: 228,
+child: Container(
+width: 162,
+height: 29,
+decoration: BoxDecoration(
+boxShadow : [BoxShadow(
+color: Color.fromRGBO(0, 0, 0, 0.25),
+offset: Offset(0,4),
+blurRadius: 4
+)],
+color : Color.fromRGBO(255, 255, 255, 1),
+)
+)
+),Positioned(
+top: 772,
+left: 286,
+child: Text('LV  Wallet', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 772,
+left: 234,
+child: Text('\$300 ', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 545,
+left: 38,
+child: Container(
+width: 168,
+height: 29,
+decoration: BoxDecoration(
+boxShadow : [BoxShadow(
+color: Color.fromRGBO(0, 0, 0, 0.25),
+offset: Offset(0,4),
+blurRadius: 4
+)],
+color : Color.fromRGBO(255, 255, 255, 1),
+)
+)
+),Positioned(
+top: 549,
+left: 101,
+child: Text('Helmet', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 549,
+left: 43,
+child: Text('\$30 ', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 767,
+left: 40,
+child: Container(
+width: 169,
+height: 29,
+decoration: BoxDecoration(
+boxShadow : [BoxShadow(
+color: Color.fromRGBO(0, 0, 0, 0.25),
+offset: Offset(0,4),
+blurRadius: 4
+)],
+color : Color.fromRGBO(255, 255, 255, 1),
+)
+)
+),Positioned(
+top: 550,
+left: 291,
+child: Text('LV  Belt', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 772,
+left: 47,
+child: Text('\$27 ', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: 772,
+left: 95,
+child: Text('Earrings', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(0, 0, 0, 0.800000011920929),
+fontFamily: 'Montserrat',
+fontSize: 15,
+letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+fontWeight: FontWeight.normal,
+height: 1.6
+),)
+),Positioned(
+top: -5,
+left: 0,
+child: SvgPicture.asset(
+'assets/images/rectangle27.svg',
+semanticsLabel: 'rectangle27'
+);
+),Positioned(
+top: 48,
+left: 38,
+child: Container(
+width: 115,
+height: 66,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Nanyangmarcheterbalik2.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 114,
+left: 38,
+child: Container(
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(50),
+topRight: Radius.circular(50),
+bottomLeft: Radius.circular(50),
+bottomRight: Radius.circular(50),
+),
+color : Color.fromRGBO(255, 255, 255, 1),
+),
+padding: EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+child: Column(
+mainAxisSize: MainAxisSize.min,
+
+children: <Widget>[Container(
+decoration: BoxDecoration(
+borderRadius : BorderRadius.only(
+topLeft: Radius.circular(10),
+topRight: Radius.circular(10),
+bottomLeft: Radius.circular(10),
+bottomRight: Radius.circular(10),
+),
+),
+padding: EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+child: Row(
+mainAxisSize: MainAxisSize.min,
+
+children: <Widget>[
+null, SizedBox(width : 0),
+Text('Search Nanyang Marché', textAlign: TextAlign.left, style: TextStyle(
+color: Color.fromRGBO(60, 60, 67, 0.6000000238418579),
+fontFamily: 'Actor',
+fontSize: 13,
+letterSpacing: -0.40799999237060547,
+fontWeight: FontWeight.normal,
+height: 1.6923076923076923
+),), SizedBox(width : 0),
+null,
+
+],
+),
+),
+],
+),
+)
+),Positioned(
+top: 53,
+left: 164,
+child: Container(
+width: 35,
+height: 38.589744567871094,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Citypng1.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),Positioned(
+top: 93,
+left: 354,
+child: Container(
+width: 33,
+height: 36,
+decoration: BoxDecoration(
+image : DecorationImage(
+image: AssetImage('assets/images/Clipart34569601.png'),
+fit: BoxFit.fitWidth
+),
+)
+)
+),
+]
+)
+);
+}
+}
+
